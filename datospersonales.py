7af3b1a450b8f6aca68b6f96b2f296e502d83407
luistavcell <159097793+luistavcell@users.noreply.github.com>
2024-02-05 22:19:40 -0500

    Add files via upload
    print(datos personales

---
+++ datospersonales.py
@@ -0,0 +1,7 @@
+name = input("ingresa tus nombres")
+last_name = input("ingrsa tusS apellidos")
+phone_numer = input("ingresa tu telefono")
+mail = input("ingresa tu mail")
+
+print(name , last_name , phone_numer , mail )
+
